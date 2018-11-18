5cc751e88b8463cf811471b5687f8e3241b7ee49
Luis Amez <luis.amez.vitoria@gmail.com>
2018-11-18 20:07:53 +0000

    Add guards to forbid access to unauthorized routes when user is logger or not

---
+++ src/app/guards/is-logged.guard.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IsLoggedGuard implements CanActivate {
+  // canActivate(
+  //   next: ActivatedRouteSnapshot,
+  //   state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+  //   return true;
+  // }
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  canActivate(): Promise<any> {
+    return this.authService.me()
+      .then((user) => {
+        if (user) {
+          return true;
+        } else {
+          this.router.navigate(['/login']);
+          return false;
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        return false;
+      });
+}
+}
