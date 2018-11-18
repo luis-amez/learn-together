d18d1a4ff560a8695ad5d311fb49c4b260f8a981
Luis Amez <luis.amez.vitoria@gmail.com>
2018-11-18 18:35:49 +0000

    Move apiUrl to an environmental variable to ease deploy and production later

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from '../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +11,6 @@ export class QuizService {
   constructor(private httpClient: HttpClient) { }
 
   getQuestions(): Promise<any> {
-    return this.httpClient.get('http://localhost:3000/question').toPromise();
+    return this.httpClient.get(`${environment.apiUrl}/question`).toPromise();
   }
 }
