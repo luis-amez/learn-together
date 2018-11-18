d3ebece5a6debe21819ec5ad038951594fb2b5fc
Luis Amez <luis.amez.vitoria@gmail.com>
2018-11-18 21:01:49 +0000

    Add chirp service with a method to retrieve all the chirps from the API

---
+++ src/app/services/chirp.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChirpService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getChirps(): Promise<any> {
+    const options = {
+      withCredentials: true
+    };
+    return this.httpClient.get(`${environment.apiUrl}/chirps`, options)
+      .toPromise();
+  }
+
+  postChirp(chirp): Promise<any> {
+    const options = {
+      withCredentials: true
+    };
+    return this.httpClient.post(`${environment.apiUrl}/auth/signup`, chirp, options)
+      .toPromise();
+  }
+}
