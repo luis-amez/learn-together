e9959b4c31391ae6bfc51819cc17170dc7b0282b
Liudvikas Taluntis <liudvikast@gmail.com>
2018-11-19 17:27:10 +0000

    Added explanation screen component

---
+++ src/app/pages/explo-screen/explo-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-explo-screen',
+  templateUrl: './explo-screen.component.html',
+  styleUrls: ['./explo-screen.component.css']
+})
+export class ExploScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
