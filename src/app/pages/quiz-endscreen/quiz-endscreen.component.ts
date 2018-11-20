17aa4388dfe624d191d0f2a130820de77b5f0d23
Liudvikas Taluntis <liudvikast@gmail.com>
2018-11-20 12:20:17 +0000

    Made endscreen display it's message and user score.

--- src/app/pages/quiz-endscreen/quiz-endscreen.component.ts
+++ src/app/pages/quiz-endscreen/quiz-endscreen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-quiz-endscreen',
@@ -7,8 +7,11 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class QuizEndscreenComponent implements OnInit {
 
-  @Output() mouseClick = new EventEmitter<boolean>();;
+  @Output() mouseClick = new EventEmitter<boolean>();
+  @Input() score: number;
 
+  quizEndText = `You'd better score more points next time or else...\n\nYour score is: `; //${this.score}
+ // \n doesn't work
   constructor() { }
 
   ngOnInit() {
